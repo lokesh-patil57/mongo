6d4ad4e498d2d779a429536d52f338957ee46409
lokesh-patil57 <luckypatil577R@gmail.com>
2025-04-29 00:13:42 +0530

    Schema in mongoose

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+const mongoose = require("mongoose");
+main()
+  .then((res) => {
+    console.log("connection Sucessful");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+}
+
+const newSchema = new mongoose.Schema ({
+    name:statusbar,
+    email:String,
+    age:Number,
+})
